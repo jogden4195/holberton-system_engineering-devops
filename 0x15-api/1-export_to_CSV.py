6a42e7221a9a2a235f88558ac9fd23342fc058f1
Jenn <jennogden95@gmail.com>
2019-02-11 23:41:11 +0000

    FIX: writes empty dict to file if user id is invalid

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,34 +15,35 @@ if __name__ == "__main__":
     # Getting employee's username
     employees = requests.get(url + 'users/' + eid)
     e_json = employees.json()
-    name = e_json['username']
+    if e_json != {}:
+        name = e_json['username']
 
-    # Getting list of employee's tasks and formatting dicts into list
-    todos = requests.get(url + 'todos/')
-    todos_dicts = todos.json()
-    e_todos = [i for i in todos_dicts if i['userId'] == int(eid)]
-    csv_todos = []
-    for task in e_todos:
-        new_dict = {}
-        new_dict['username'] = name
-        new_dict['user_id'] = int(eid)
-        new_dict['task_completed_status'] = task['completed']
-        new_dict['task_title'] = task['title']
-        csv_todos.append(new_dict)
+        # Getting list of employee's tasks and formatting dicts into list
+        todos = requests.get(url + 'todos/')
+        todos_dicts = todos.json()
+        e_todos = [i for i in todos_dicts if i['userId'] == int(eid)]
+        csv_todos = []
+        for task in e_todos:
+            new_dict = {}
+            new_dict['username'] = name
+            new_dict['user_id'] = int(eid)
+            new_dict['task_completed_status'] = task['completed']
+            new_dict['task_title'] = task['title']
+            csv_todos.append(new_dict)
 
-    # Exporting into CSV
-    csv_columns = ['user_id',
-                   'username',
-                   'task_completed_status',
-                   'task_title']
-    csv_file = eid + '.csv'
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile,
-                                    fieldnames=csv_columns,
-                                    quoting=csv.QUOTE_ALL)
-            writer.writeheader()
-            for data in csv_todos:
-                writer.writerow(data)
-    except IOError:
-        print("I/O error")
+        # Exporting into CSV
+        csv_columns = ['user_id',
+                       'username',
+                       'task_completed_status',
+                       'task_title']
+        csv_file = eid + '.csv'
+        try:
+            with open(csv_file, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile,
+                                        fieldnames=csv_columns,
+                                        quoting=csv.QUOTE_ALL)
+                writer.writeheader()
+                for data in csv_todos:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
