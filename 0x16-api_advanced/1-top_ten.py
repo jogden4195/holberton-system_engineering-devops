359a71b3ae99181c4f6eae79d6b965b88519c07d
Jenn <jennogden95@gmail.com>
2019-02-14 01:22:21 +0000

    FIX: Changed check for when get fails

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,6 +2,7 @@
 """
 Module for top_ten
 """
+import requests
 
 
 def top_ten(subreddit):
@@ -9,8 +10,6 @@ def top_ten(subreddit):
     Queries the Reddit API and prints the titles of the first 10
     hot posts listed for a given subreddit
     """
-    import requests
-
     headers = {
         'User-Agent': 'Mozilla/5.0 '
         '(Macintosh; Intel Mac OS X 10_10_1) '
@@ -20,13 +19,13 @@ def top_ten(subreddit):
         'Content-Type': 'application/json'}
     url = 'http://www.reddit.com/r/' + subreddit + '/hot.json'
     data = requests.get(url, headers=headers)
-    if not data:
+    if data.status_code != 200:
         print('None')
-        return 0
+        return
     data_json = data.json()
     posts = data_json['data']['children']
     if len(posts) == 0:
         print('None')
-        return 0
+        return
     for p in posts[:9]:
         print(p['data']['title'])
